e9ec3cf1cc9364b823846ff8d9fbab9a82447381
vormc2005 <voronov.dmitry@gmail.com>
2020-04-05 14:40:01 -0400

    corrected responsiveness

--- src/components/Aboutme/ABoutme.js
+++ src/components/Aboutme/ABoutme.js
@@ -15,8 +15,8 @@ const Aboutme =()=>{
                         <div className="card-image">
                             <img class="responsive-img" src={myImage} alt="My_image"/>
                         </div>
-                <div className="card-stacked">
-                    <div className="card-content">
+                <div className="card-stacked col s12">
+                    <div className="card-content ">
                              <p>My name is Dmitry Voronov. I am a full stack web developer. I build responsive   dynamic  web applications using Javascript, JQuery, React, NodeJS. I am also familiar with Mongo, MySQL, Express, Express-Handlebars. 
                             I obtained developer skills from George Washington Universitys' full stack developer certification course. In addition, I have a degree in Finance from George Mason University, and Business Administration degree from NVCC. 
                         One of the reasons why I chose to learn codding is that I like to solve problems and create solutionsthat make peoples' life easier. I like learning new skills and push myself to new limits.</p>
