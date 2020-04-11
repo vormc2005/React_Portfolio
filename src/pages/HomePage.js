6ad00ba4c21a85b6cc239ac85c4edbdde6f87410
vormc2005 <voronov.dmitry@gmail.com>
2020-04-11 14:29:03 -0400

    Parallax +portfolio

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from "../components/Navbar/Navbar"
 import Aboutme from "../components/Aboutme/ABoutme"
 import Footer from '../components/Footer/Footer';
 import Techcard from "../components/TechCard/Techcard"
 import Parallax from "../components/Parallax/Parallax"
-import Style from "./style.css"
 
 
 
-function HomePage() {
+import Wrapper from "../components/Wrapper/Wrapper"
+import projects from "../../src/projects.json"
+import PortfolioCard from "../components/PortfolioCard/portfoliocard"
 
-  
+
+
+class HomePage extends Component {
+  state ={
+      projects
+  }
+  render(){
   return (
     <>
     
@@ -23,6 +30,26 @@ function HomePage() {
      
       <Techcard/>
       <Parallax/>
+      <Wrapper> 
+           
+           {this.state.projects.map( project=> (
+             
+               <PortfolioCard            
+                 id={project.id}
+                 key={project.id}
+                 name={project.name}
+                 image={project.image}
+                 technology={project.technology}
+                 description={project.description}
+                 Link ={project.Link}
+                 
+               />
+               
+             ))}        
+           
+          
+           </Wrapper>
+      <Parallax/>
      
       <Footer/>
 
@@ -31,6 +58,7 @@ function HomePage() {
    
   </>
   );
+           }
 }
 
 export default HomePage;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from "../components/Navbar/Navbar"
 import Footer from '../components/Footer/Footer';
 import ContactForm from '../components/Aboutme/ContactForm.js/ContactForm';
-
+import "./style.css"
 
 
 function Contact() {
