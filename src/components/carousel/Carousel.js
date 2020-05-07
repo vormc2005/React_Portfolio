daeb2055b9c015c29dde999ee3da035453cab7b1
Dmitry Voronov <voronov.dmitry@gmail.com>
2020-05-07 11:48:01 -0400

    added caousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import "./style.css"
-import image1 from '../Images/bidbot.gif'
-import image2 from '../Images/Github_finder.gif'
-import image3 from '../Images/Recipe_boss.gif'
-import image4 from '../Images/Budget_App.gif'
-import image5 from '../Images/busybot.gif'
+import image1 from '../Images/bidbot.gif';
+import image2 from '../Images/Github_finder.gif';
+import image3 from '../Images/Recipe_boss.gif';
+import image4 from '../Images/Budget_App.gif';
+import image5 from '../Images/busybot.gif';
+import image6 from '../Images/Piggame.gif';
+import image7 from '../Images/weather.gif'
+
  
 class DemoCarousel extends Component {
     render() {
@@ -29,12 +32,22 @@ class DemoCarousel extends Component {
                 <div>
                     <img src={image4} alt="
                     4" />
-                    <p className="legend">BusyBot - find events near you</p>
+                    <p className="legend">Budgt App - input your income and expenses and calculate your budget</p>
                 </div>
                 <div>
                     <img src={image5} alt="
                     5" />
-                    <p className="legend">Legend 3</p>
+                    <p className="legend">BusyBot - find events near you</p>
+                </div>
+                <div>
+                    <img src={image6} alt="
+                    6" />
+                    <p className="legend">Pig Game - classic roll the dice game</p>
+                </div>
+                <div>
+                    <img src={image7} alt="
+                    7" />
+                    <p className="legend">Weather Service Application</p>
                 </div>
             </Carousel>
         );
