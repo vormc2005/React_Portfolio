db8d5e79f3a10d6d0559316ae83c0d6c96d66c54
vormc2005 <voronov.dmitry@gmail.com>
2020-11-14 09:49:15 -0500

    Edited tech card

--- src/components/TechCard/Techcard.js
+++ src/components/TechCard/Techcard.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import mySites from './mySites.json'
+
 import "./Techcard.css"
 import javaS from "../../Images/javascript.png"
 import angular from "../../Images/angularjs.png"
@@ -14,95 +16,109 @@ import html from "../../Images/html.png"
 import css from "../../Images/css.png"
 
 
+
 const Techcard = () => {
+
+   
 
     return (
         <>
-            <div className=" myinfo ">
-                <div className="col-sm-12 ">
-                    <div className="techcard  " id="techcard">
-                        {/* <div className="row"> */}
-
-                        <div >
-                            <div className="card-content">
-                                {/* <div className="col "> */}
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <h3 id="skillsheader">Technology Stack I Use</h3>
-                                    </div>
-
-                                </div>
-
-
-        <div className=" row">
-            <div className="col">
-                <ul className="skillslist" >
-                    <p
+                  
+                    <div className="techcard"   id="techcard">                                                       
+                     <div className="row">
+                        <div className="col-md-12">
+                           <h3 id="skillsheader">Technology Stack I Use</h3>
+                      </div>
+                    </div>
+            <div className="row" >           
+                <div className="col-md-3 skill-div-left"  >
+                    <p className="skillslist-left"
                         style={{ listStyleType: "disc" }}>
                         <span>
                             <img src={javaS} alt="javaS" style ={{maxHeight:"25px"}}/>
                         </span> JavaScript
                     </p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-left"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={html} alt="html" style ={{maxHeight:"25px"}}/>
                         </span> HTML
                     </p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-left"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={css} alt="css" style ={{maxHeight:"25px"}}/>
                         </span>CSS</p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-left"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={node} alt="node" style ={{maxHeight:"25px"}}/>
                         </span> NodeJS</p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-left"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={react} alt="react" style ={{maxHeight:"25px"}}/>
                         </span> ReactJS</p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    {/* <p className="skillslist-left" 
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={mongoDb} alt="mongoDb" style ={{maxHeight:"25px"}}/>
                             <img src={react} alt="react" style ={{maxHeight:"25px"}}/>
-                        </span> MERN stack application</p>
-                        <p style={{ listStyleType: "disc" }}> <span>
+                        </span> MERN stack application</p> */}
+                        <p className="skillslist-left"
+                        style={{ listStyleType: "disc" }}> <span>
                             <img src={jquery} alt="jquery" style ={{maxHeight:"25px"}}/>
                            
-                        </span> Jquery</p>
-                  
-                  
-                </ul>
-            </div>
-
-
-            <div className=" col ">
-                <ul className="skillslist">
-                    <p style={{ listStyleType: "disc" }}> <span>
+                        </span> Jquery</p>  
+               
+                </div>
+                <div className="col-md-6" >
+                    <div className="row" >
+                {mySites.map(mysite=>{
+                    return( 
+                            <div className="col-md-6" style={{marginLeft:"0vh"}}>   
+                                <div className="dark-bg">
+                                    <p className="project-text">{mysite.type}</p>
+                                </div>
+                        </div> 
+                        
+                        )
+                    })}
+                    </div>
+                    </div>
+                {/* <div>
+                    <div className="dark-bg">
+                        <p className="project-text">Ecommerce applications</p>
+                    </div>
+                </div> */}
+          
+            
+                <div className="col-md-3 skill-div-right">
+                    <p className="skillslist-right" style={{ listStyleType: "disc" }}> <span>
                             <img src={angular} alt="angular" style ={{maxHeight:"25px"}}/>
                         </span> Angular 8</p>
-                    <p
+                    <p className="skillslist-right"
                         style={{ listStyleType: "disc" }}>
                         <span>
                             <img src={typescript} alt="typescript" style ={{maxHeight:"25px"}}/>
                         </span> TypeScript</p>
-                    <p
+                    <p className="skillslist-right"
                         style={{ listStyleType: "disc" }}>
                         <span>
                             <img src={xml} alt="xml" style ={{maxHeight:"25px"}}/>
                         </span> XML
                     </p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-right"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={mongoDb} alt="mongoDb" style ={{maxHeight:"25px"}}/>
                         </span> MongoDB</p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-right"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={mysql} alt="mysql" style ={{maxHeight:"25px"}}/>
                         </span> MySQL</p>
-                    <p style={{ listStyleType: "disc" }}> <span>
+                    <p className="skillslist-right"
+                    style={{ listStyleType: "disc" }}> <span>
                             <img src={bootstrap} alt="bootstrap" style ={{maxHeight:"25px"}}/>
-                        </span> Bootstrap</p>
-                   
-                </ul>
-
-            </div>
-        </div>
-
-                            </div>
-                        </div>
+                        </span> Bootstrap</p>                   
+                </div>
+            </div>        
+        
+                        
                         <div className="row">
                             <button className="btn btn-primary " id="techBtn">
                                 <a id="myportfoliobtn" href="/portfolio">My Portfolio</a>
@@ -110,8 +126,8 @@ const Techcard = () => {
                         </div>
 
                     </div>
-                </div>
-            </div>
+               
+          
         </>
     )
 }
--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -4,96 +4,64 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './carousel.css'
 
 
-
-import image1 from "./Images/bidbot.gif";
-import image2 from './Images/Budget_App.gif';
-import image3 from './Images/busybot.gif';
-import image4 from './Images/Github_finder.gif';
-import image5 from './Images/Pig_game.gif';
-import image6 from './Images/Recipe_boss.gif';
-
-
  
 class BootstrapCarousel extends Component {
     render() {
         return (
-          <div className="aroundcarousel">
+          <div className="container-fluid aroundcarousel">
             <Carousel>
+
             <Carousel.Item>
               <img style={{maxheight:"520"}}
                 className="d-block w-100"
-                src={image1}
+                src='./Images/bidbot.gif'
                 alt="First slide"
-              />
-              {/* <Carousel.Caption>
-                <h3>Bid Bot</h3>
-                <p>Auction Application - E-comerce</p>
-              </Carousel.Caption> */}
+              />            
             </Carousel.Item>
+
             <Carousel.Item>
               <img
                 style={{maxheight:"520"}}
                 className="d-block w-100"
-                src={image2}
+                src='./Images/Budget_App.gif'
                 alt="2 slide"
-              />
-          
-              {/* <Carousel.Caption >
-                <h3 >Budget Application</h3>
-                <p >Calculate your budget</p>
-              </Carousel.Caption> */}
+              />           
             </Carousel.Item>
+
             <Carousel.Item>
               <img
                 style={{maxheight:"520"}}
                 className="d-block w-100"
-                src={image3}
+                src='./Images/busybot.gif'
                 alt="Third slide"
-              />
-          
-              {/* <Carousel.Caption>
-                <h3>BusyBot</h3>
-                <p>Find events near you</p>
-              </Carousel.Caption> */}
+              />              
             </Carousel.Item>
+
             <Carousel.Item>
               <img
                 style={{maxheight:"520"}}
                 className="d-block w-100"
-                src={image4}
+                src='./Images/Github_finder.gif'
                 alt="4 slide"
-              />
-          
-              {/* <Carousel.Caption>
-                <h3>Github User Finder</h3>
-                <p>Find git hub user</p>
-              </Carousel.Caption> */}
+              />            
             </Carousel.Item>
+
             <Carousel.Item>
               <img
                 style={{height:"300"}}
                 className="d-block w-100"
-                src={image5}
+                src='./Images/Pig_game.gif'
                 alt="5th slide"
-              />
-          
-              {/* <Carousel.Caption>
-                <h3>Cassic Pig Game</h3>
-                <p>Classic rool the dice game</p>
-              </Carousel.Caption> */}
+              />            
             </Carousel.Item>
+
             <Carousel.Item>
               <img
                 style={{maxheight:"520"}}
                 className="d-block w-100"
-                src={image6}
+                src='./Images/Recipe_boss.gif'
                 alt="6th slide"
-              />
-          
-              {/* <Carousel.Caption>
-                <h3>Forkify</h3>
-                <p>Search for recipes</p>
-              </Carousel.Caption> */}
+              />           
             </Carousel.Item>
            
           </Carousel>
