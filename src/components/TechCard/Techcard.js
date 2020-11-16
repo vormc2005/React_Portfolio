a08624582ae1d61ea757a3e4abd385758c4c629e
vormc2005 <voronov.dmitry@gmail.com>
2020-11-15 20:32:01 -0500

    fadf

--- src/components/TechCard/Techcard.js
+++ src/components/TechCard/Techcard.js
@@ -26,12 +26,12 @@ const Techcard = () => {
         <>
                   
                     <div className="techcard"   id="techcard">                                                       
-                     <div className="row " style={{width:"99.8%"}}>
+                     <div className="row " style={{width:"100%"}}>
                         <div className="col-md-12">
                            <h3 id="skillsheader">My Tech Stack and Solutions</h3>
                       </div>
                     </div>
-            <div className="row" style={{width:"99.8%"}}>           
+            <div className="row" style={{width:"100%"}}>           
                 <div className="col-md-3 skill-div-left"  >
                     <p className="skillslist-left"
                         style={{ listStyleType: "disc" }}>
@@ -69,7 +69,7 @@ const Techcard = () => {
                
                 </div>
                 <div className="col-md-6" >
-                    <div className="row" style={{width:"99.8%"}}>
+                    <div className="row" >
                 {mySites.map(mysite=>{
                     return( 
                             <div className="col-md-6" style={{marginLeft:"0vh"}}>   
@@ -120,9 +120,9 @@ const Techcard = () => {
             </div>        
         
                         
-                        <div className="row" style={{width:"99.8%"}}>
+                        <div className="row" style={{width:"100%"}}>
                             <button className="btn btn-primary " id="techBtn">
-                                <a id="myportfoliobtn" href="/portfolio">My Portfolio</a>
+                                <a id="myportfoliobtn" href="/portfolio">Click here to view my portfolio</a>
                             </button>
                         </div>
                         
